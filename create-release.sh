8511394b5876c703ff2a48da9219512d058b5afe
Alexey A. Stepanenko <alexey.stepanenko@gmail.com>
2026-02-22 15:44:27 +0300

    issue: #66 1. Добавил скрипт работы с релизами. Сейчас реализована только однка команда Создать 2. Удалил старую версию скрипта создания релиза

---
+++ create-release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# == НАЧАЛО БЛОКА ИЗМЕНЯЕМЫХ ПАРАМЕТРОВ ==
+
+# Путь к файлу с настройками оносительно каталога репозитория
+# Например:
+# conf="conf/my-conf.json"
+conf="conf/conf.template.json"
+
+# == КОНЕЦ БЛОКА ИЗМЕНЯЕМЫХ ПАРАМЕТРОВ ==
+
+# Путь к файлу запуска интерпретатора OneScript, может понадобиться установка абсолютного пути
+oscript_engine="oscript"
+
+script="releases.os"
+command="create"
+repo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+exec="$oscript_engine \"$repo/scripts/$script\" $command -conf \"$conf\" -create-number-release-dir"
+
+eval "$exec"
